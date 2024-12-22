4479166e79d54e4ce756b93cb0cd86146be148a0
red-jen <redaje102105@gmail.com>
2024-12-22 02:12:35 +0100

    footer

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer class="bg-gray-800 text-white p-4 mt-8">
+    <div class="container mx-auto text-center">
+        <p>&copy; <?php echo date("Y"); ?> V-BLOGS. All rights reserved.</p>
+    </div>
+</footer>
