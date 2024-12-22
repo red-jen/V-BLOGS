9b95fa6adec707a42a967aeb347ec0fbb15ff713
red-jen <redaje102105@gmail.com>
2024-12-22 14:05:13 +0100

    url right path of image

--- functions.php
+++ functions.php
@@ -22,4 +22,35 @@ function requireLogin() {
         exit();
     }
 }
+
+
+function getImageUrl($imagePath) {
+    if (empty($imagePath)) {
+        return 'placeholder.jpg';
+    }
+    
+    // Check if the image path is absolute or relative
+    if (strpos($imagePath, 'http') === 0) {
+        return $imagePath;
+    }
+    
+    // Convert backslashes to forward slashes
+    $imagePath = str_replace('\\', '/', $imagePath);
+    
+    // Remove any leading slash
+    $imagePath = ltrim($imagePath, '/');
+    
+    // Get the base URL of your website
+    $baseUrl = sprintf(
+        "%s://%s%s",
+        isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
+        $_SERVER['SERVER_NAME'],
+        dirname($_SERVER['SCRIPT_NAME'])
+    );
+    
+    // Remove any trailing slash from base URL
+    $baseUrl = rtrim($baseUrl, '/');
+    
+    return $baseUrl . '/' . $imagePath;
+}
 ?>
